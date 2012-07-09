56fab61de53df18e764edc85c72d438005bb3233
antonevane <antonevane@gmail.com>
2012-07-09 16:47:45 +0400

    Chapter 9 has been introduced

---
+++ src/ru/antonevane/ch9/MyThread.java
@@ -0,0 +1,22 @@
+package ru.antonevane.ch9;
+
+public class MyThread extends Thread {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		MyThread myThread = new MyThread();
+
+		myThread.start();
+		myThread.sleep(10);
+
+	}
+
+	@Override
+	public void run() {
+		System.out.println("MyThread extend Thread");
+	}
+
+}
