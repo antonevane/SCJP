8a7dac04a7162d0626aeeafcf2a248db30bb8c69
antonevane <antonevane@gmail.com>
2012-07-11 15:24:48 +0400

    String notes has been added

--- src/ru/antonevane/ch1/pr2/NonChild.java
+++ src/ru/antonevane/ch1/pr2/NonChild.java
@@ -1,10 +1,9 @@
 package ru.antonevane.ch1.pr2;
 
 public class NonChild {
-	
 	public static void main(String[]... args) {
 		Child child = new Child();
-//		System.out.println(child.x);
-//		System.out.println(child.protectedMethod());
+		// System.out.println(child.x);
+		// System.out.println(child.protectedMethod());
 	}
 }
